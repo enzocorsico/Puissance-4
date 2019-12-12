bf37c7bab4418807fcb88d295670fa48dc982101
Destroyer68 <58816644+Destroyer68@users.noreply.github.com>
2019-12-12 21:50:51 +0100

    Pions fini
    Les pions marchent et se mettent en place, il faut encore faire les couleurs et la gestions de manche gagné

--- Puissance 4.py
+++ Puissance 4.py
@@ -3,6 +3,7 @@ colonnex=[-150,-100,-50,0,50,100,150]
 ligney=[-175,-125,-75,-25,25,75]
 colonne=0
 ligne=0
+joueur=1
 pion=False
 colonne1=[0,0,0,0,0,0]
 colonne2=[0,0,0,0,0,0]
@@ -59,7 +60,10 @@ def Coup_possible():
             ligne=1
             pion=True
             return
-        
+        if colonne1[0]!=0 and colonne1[1]!=0 and colonne1[2]!=0 and colonne1[3]!=0 and colonne1[4]!=0 and colonne1[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==1:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
         for d in range(len(colonne1)):
             if colonne1[d]!=0:
                 pion=False
@@ -68,8 +72,120 @@ def Coup_possible():
                 ligne=d+1
                 pion=True
                 return
+    
+    if colonne==2:
+        if colonne2[0]+colonne2[1]+colonne2[2]+colonne2[3]+colonne2[4]+colonne2[5]==0:
+            colonne2[0]=1
+            ligne=1
+            pion=True
+            return
+        if colonne2[0]!=0 and colonne2[1]!=0 and colonne2[2]!=0 and colonne2[3]!=0 and colonne2[4]!=0 and colonne2[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==2:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
+        for d in range(len(colonne2)):
+            if colonne2[d]!=0:
+                pion=False
+            else:
+                colonne2[d]=1
+                ligne=d+1
+                pion=True
+                return
+    
+    if colonne==3:
+        if colonne3[0]+colonne3[1]+colonne3[2]+colonne3[3]+colonne3[4]+colonne3[5]==0:
+            colonne3[0]=1
+            ligne=1
+            pion=True
+            return
+        if colonne3[0]!=0 and colonne3[1]!=0 and colonne3[2]!=0 and colonne3[3]!=0 and colonne3[4]!=0 and colonne3[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==3:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
+        for d in range(len(colonne3)):
+            if colonne3[d]!=0:
+                pion=False
+            else:
+                colonne3[d]=1
+                ligne=d+1
+                pion=True
+                return
             
-
+    if colonne==4:
+        if colonne4[0]+colonne4[1]+colonne4[2]+colonne4[3]+colonne4[4]+colonne4[5]==0:
+            colonne4[0]=1
+            ligne=1
+            pion=True
+            return
+        if colonne4[0]!=0 and colonne4[1]!=0 and colonne4[2]!=0 and colonne4[3]!=0 and colonne4[4]!=0 and colonne4[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==4:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
+        for d in range(len(colonne4)):
+            if colonne4[d]!=0:
+                pion=False
+            else:
+                colonne4[d]=1
+                ligne=d+1
+                pion=True
+                return
+    
+    if colonne==5:
+        if colonne5[0]+colonne5[1]+colonne5[2]+colonne5[3]+colonne5[4]+colonne5[5]==0:
+            colonne5[0]=1
+            ligne=1
+            pion=True
+            return
+        if colonne5[0]!=0 and colonne5[1]!=0 and colonne5[2]!=0 and colonne5[3]!=0 and colonne5[4]!=0 and colonne5[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==5:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
+        for d in range(len(colonne5)):
+            if colonne5[d]!=0:
+                pion=False
+            else:
+                colonne5[d]=1
+                ligne=d+1
+                pion=True
+                return
+    
+    if colonne==6:
+        if colonne6[0]+colonne6[1]+colonne6[2]+colonne6[3]+colonne6[4]+colonne6[5]==0:
+            colonne6[0]=1
+            ligne=1
+            pion=True
+            return
+        if colonne6[0]!=0 and colonne6[1]!=0 and colonne6[2]!=0 and colonne6[3]!=0 and colonne6[4]!=0 and colonne6[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==6:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
+        for d in range(len(colonne6)):
+            if colonne6[d]!=0:
+                pion=False
+            else:
+                colonne6[d]=1
+                ligne=d+1
+                pion=True
+                return
+            
+    if colonne==7:
+        if colonne7[0]+colonne7[1]+colonne7[2]+colonne7[3]+colonne7[4]+colonne7[5]==0:
+            colonne7[0]=1
+            ligne=1
+            pion=True
+            return
+        if colonne7[0]!=0 and colonne7[1]!=0 and colonne7[2]!=0 and colonne7[3]!=0 and colonne7[4]!=0 and colonne7[5]!=0:
+            pion=False
+            while colonne<1 or colonne>7 or colonne==7:
+                colonne=int(input("Veuillez ressaisir une colonne valide: "))
+        for d in range(len(colonne7)):
+            if colonne7[d]!=0:
+                pion=False
+            else:
+                colonne7[d]=1
+                ligne=d+1
+                pion=True
+                return
 
 
 
@@ -87,7 +203,8 @@ def cercle():
 mise_en_place()
 Grille_vide()
 
-for w in range(5):
+z=100
+for w in range(z):
     Coup_possible()
     if pion==True:
         cercle()
